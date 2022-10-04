8609a9a871333acea8a8ce19cef4d2a01e140476
Robin_Chen <sudacgb@gmail.com>
2022-10-04 11:32:50 -0400

    add test proof of work part

--- backend/tests/blockchain/test_block.py
+++ backend/tests/blockchain/test_block.py
@@ -1,4 +1,7 @@
+import time
+
 from backend.blockchain.block import Block, GENESIS_DATA
+from backend.config import MINE_RATE, SECONDS
 
 def test_mine_block():
 	last_block = Block.genesis()
@@ -9,6 +12,7 @@ def test_mine_block():
 	assert isinstance(block, Block)
 	assert block.data == data 
 	assert block.last_hash == last_block.hash
+	assert block.hash[0:block.difficulty] == '0' * block.difficulty
 
 
 def test_genesis():
@@ -23,4 +27,28 @@ def test_genesis():
 	# more concise code
 	for key, value in GENESIS_DATA.items():
 		getattr(genesis, key) == value
+	
+
+def test_quickly_mined_block():
+	last_block = Block.mine_block(Block.genesis(), 'foo')
+	time.sleep(MINE_RATE // SECONDS) # 4s delay now 
+	mined_block = Block.mine_block(last_block, 'bar')
+
+	assert mined_block.difficulty == last_block.difficulty - 1 
+
+
+def test_mined_block_difficulty_limits_at_1():
+	last_block = Block(
+		time.time_ns(),
+		'test_last_hash', 
+		'test_hash', 
+		'test_data',
+		1,
+		0
+	)
+
+	time.sleep(MINE_RATE / SECONDS)
+	mined_block = Block.mine_block(last_block, 'bar')
+
+	assert mined_block.difficulty == 1
 	
