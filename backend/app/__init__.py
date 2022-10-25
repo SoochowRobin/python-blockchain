86ccfeb7dec78ad506b084915cf4aef8f6a5385a
Robin_Chen <sudacgb@gmail.com>
2022-10-24 20:22:08 -0400

    add peer instances

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,3 +1,5 @@
+import os
+import random
 from flask import Flask, jsonify
 
 from backend.blockchain.blockchain import Blockchain
@@ -22,4 +24,9 @@ def route_blockchain_mine():
 
 	return jsonify(blockchain.chain[-1].to_json())
 
-app.run()
+PORT = 5000
+
+if os.environ.get('PEER') == 'True':
+	PORT = random.randint(5001, 6000)
+	
+app.run(port=PORT)
